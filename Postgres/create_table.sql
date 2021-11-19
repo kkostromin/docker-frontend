5441461acf9f8ac206556e55a2391bb7ef470f4f
Konstantin Kostromin <59706484+kkostromin@users.noreply.github.com>
2021-11-19 18:58:39 +0300

    Delete create_table.sql

--- Postgres/create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS AIRFLOW_DAGS (           
-  message varchar(255) NOT NULL         
-  );          
-  INSERT INTO AIRFLOW_DAGS(message) VALUES('Ansible is fun');
-ALTER TABLE AIRFLOW_DAGS OWNER TO "AIRFLOW";
-
